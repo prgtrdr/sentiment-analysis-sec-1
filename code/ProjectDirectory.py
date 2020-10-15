93a2f65d4a01198ddd8f585f541f88f16f5dc324
prgtrdr <bill@hartsandco.com>
2020-10-15 16:28:06 -0400

    various

--- code/ProjectDirectory.py
+++ code/ProjectDirectory.py
@@ -1,11 +1,11 @@
 import os
-from pathlib2 import Path
+from pathlib import Path
 
 
 
 def get_project_dir():
     try:
-        project_dir = Path.cwd() / '/' / 'My Drive' / 'Jotham' / 'Personal Docs' / 'ML for finance' / 'SEC Sentiment Analysis - Github Upload' / 'sec-sentiment'
+        project_dir = Path.cwd() / '/' / 'Users' / 'bill' / 'Documents' / 'Development' / 'sentiment-analysis-sec'
         os.chdir(project_dir)
     except BaseException as e:
         project_dir = Path.cwd() / '/' / 'Volumes' / 'GoogleDrive' / 'My Drive' / 'Jotham' / 'Personal Docs' / 'ML for finance' / 'SEC Sentiment Analysis - Github Upload' / 'sec-sentiment'
